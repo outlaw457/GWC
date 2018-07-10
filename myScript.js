59cd7e22b261f946d738629760f4603b121ec8ba
outlaw457 <41089250+outlaw457@users.noreply.github.com>
2018-07-10 15:24:40 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,9 @@
+// js scripts for web page
+
+function myFunction(){
+  alert("Page is loaded.");
+}
+
+<img src="woah.jpg" width="100" height="100" name="image_1"
+onmouseover="image_1.width='300';image_1.height='300';"
+onmouseout="image_1.width='100';image_1.height='100';" />
